b901f583324ce06d0b54324c869df20d16282d2a
besirThor <besir.kurtishi@thorindustriesmk.com>
2023-04-06 15:18:31 +0200

    first commit

---
+++ types.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+type CreateAccountRequest struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+type Account struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Number    int64     `json:"number"`
+	Balance   int64     `json:"balance"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+func NewAccount(firstname string, lastname string) *Account {
+	return &Account{
+		FirstName: firstname,
+		LastName:  lastname,
+		Number:    int64(rand.Intn(1000000)),
+		CreatedAt: time.Now().UTC(),
+	}
+}
